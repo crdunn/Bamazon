cbc2bd554d7dc02e541293a7d131c74ff4b55b3f
Charles Dunn <charlesdunn@Charless-MacBook-Pro.local>
2017-10-23 14:21:52 -0400

    added seeds

--- bamazon.sql
+++ bamazon.sql
@@ -16,3 +16,14 @@ CREATE TABLE products (
     
 	stock_quantity INT(5)
 );
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('26843','Home Reference Guide','Books','16','200');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('52934','Bananas','Food','5','500');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('90436','Laptop','Electronics','900','50');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('35832','Javascript Textbook','Books','50','300');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('23464','Television','Electronics','500','75');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('74376','Bestseller','Books','35,5000');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('18346','Hammer','Hardware','25','100');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('33846','Screwdriver','Hardware','15','30');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('48382','PSYUPS','Psyonic Enhancers','100','50');
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) values ('84012','Construction Set','Toys','45','150');
